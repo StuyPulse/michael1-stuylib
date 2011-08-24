009ed5277044bf779158b8e9361bbde3c6d5af68
Alejandro Carrillo <aecarrillo594@gmail.com>
2011-08-24 19:47:22 -0400

    Added src files

---
+++ src/edu/wpi/first/wpilibj/templates/Michael1Stuylib.java
@@ -0,0 +1,46 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package edu.wpi.first.wpilibj.templates;
+
+
+import edu.wpi.first.wpilibj.*;
+
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the SimpleRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Michael1Stuylib extends SimpleRobot {
+    /**
+     * This function is called once each time the robot enters autonomous mode.
+     */
+
+    DashboardUpdater du;
+    UserInput in;
+    DriveConstants config;
+
+    public RobotTemplate() {
+        config = new UserInputConfig();
+        in = new UserInput(config);
+        du = new DashboardUpdater(this);
+    }
+    public void autonomous() {
+        
+    }
+
+    /**
+     * This function is called once each time the robot enters operator control.
+     */
+    public void operatorControl() {
+
+    }
+
+}
---
+++ src/edu/wpi/first/wpilibj/templates/UserInputConfig.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ *
+ * @author Ginkgo
+ */
+public class UserInputConfig implements DriveConstants{
+
+
+    int getLeftStickPort() {
+        return 1;
+    }
+    int getRightStickPort() {
+        return 2;
+    }
+    int getExtraStickPort1() {
+        return 3;
+    }
+    int getExtraStickPort2() {
+        return 4;
+    }
+
+}
